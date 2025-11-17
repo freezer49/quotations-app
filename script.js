390053ff17e447037d18e43ed179a48831e2c5a6
Chloe Verglas <chloe.verglas@gmail.com>
2025-11-17 11:58:43 +0100

    display-quote-page

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //Ajout d'un gestionnaire d'evenement avec addEventListener
 
 const quoteForm = document.getElementById("quoteForm");
-
 //Récupérez les valeurs des champs <input> dans les deux variables
 
 quoteForm.addEventListener("submit", function (event) {
@@ -13,4 +12,25 @@ quoteForm.addEventListener("submit", function (event) {
 
   console.log("Citation :", text);
   console.log("Auteur :", author);
+
+  addQuote(text, author);
 });
+
+//Affichage des citations saisies directement sur la page.
+
+function addQuote(quote, author) {
+  const quoteText = document.createElement("p");
+  quoteText.classList.add("text");
+  quoteText.textContent = quote;
+
+  const authorText = document.createElement("p");
+  authorText.classList.add("author");
+  authorText.textContent = author;
+
+  const quoteDiv = document.createElement("div");
+  quoteDiv.appendChild(quoteText);
+  quoteDiv.appendChild(authorText);
+
+  const quoteList = document.getElementById("quote-list");
+  quoteList.appendChild(quoteDiv);
+}
